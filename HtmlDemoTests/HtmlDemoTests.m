9ef0a1198e1f9de8009096b12ffadefd0de6bac9
Mr Song <song@wwdeimac.local>
2017-02-15 10:44:17 +0800

    Initial Commit

---
+++ HtmlDemoTests/HtmlDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  HtmlDemoTests.m
+//  HtmlDemoTests
+//
+//  Created by MrSong on 17/2/15.
+//  Copyright © 2017年 song. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HtmlDemoTests : XCTestCase
+
+@end
+
+@implementation HtmlDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
